ae6b4fa4704900df9cdcbe7b3a98a86771daa98f
Luiz <luiz504@hotmail.com>
2024-01-03 18:02:09 -0300

    feat: test factories faker integration

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,13 +1,20 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from '~/core/entities/unique-entity-id'
 import { Question } from '~/domain/forum/enterprise/entities/question'
 
-export function makeQuestion(override: Partial<Question> = {}) {
-  const newQuestion = Question.create({
-    authorId: new UniqueEntityID('some-author-id'),
-    title: 'Question Title Example',
-    content: 'Question Content Example',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<Question> = {},
+  id?: UniqueEntityID,
+) {
+  const newQuestion = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return newQuestion
 }
