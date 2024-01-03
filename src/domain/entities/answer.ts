ede8a3bd59d3ba619050cdfcf2eb1476902af1af
Luiz <luiz504@hotmail.com>
2024-01-03 13:28:26 -0300

    feat: entities classes refactor

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from 'node:crypto'
+import { Entity } from '../../core/entities/entity'
 
 interface AnswerProps {
   content: string
@@ -6,16 +6,8 @@ interface AnswerProps {
   questionId: string
 }
 
-export class Answer {
-  public id: string
-  public content: string
-  public authorId: string
-  public questionId: string
-
-  constructor({ questionId, authorId, content }: AnswerProps, id?: string) {
-    this.content = content
-    this.authorId = authorId
-    this.questionId = questionId
-    this.id = id ?? randomUUID()
+export class Answer extends Entity<AnswerProps> {
+  get content() {
+    return this.props.content
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,5 +1,5 @@
-import { randomUUID } from 'node:crypto'
 import { Slug } from './value-objects/slug'
+import { Entity } from '../../core/entities/entity'
 
 interface QuestionProps {
   title: string
@@ -7,18 +7,4 @@ interface QuestionProps {
   slug: Slug
   authorId: string
 }
-export class Question {
-  public id: string
-  public title: string
-  public slug: Slug
-  public content: string
-  public authorId: string
-
-  constructor({ title, slug, content, authorId }: QuestionProps, id?: string) {
-    this.title = title
-    this.slug = slug
-    this.content = content
-    this.authorId = authorId
-    this.id = id ?? randomUUID()
-  }
-}
+export class Question extends Entity<QuestionProps> {}
