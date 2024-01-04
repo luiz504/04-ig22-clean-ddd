f56f8a731cc3993d9618d91ea7d39eb2112107ca
Luiz <luiz504@hotmail.com>
2024-01-04 16:16:56 -0300

    feat: comment class base split

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,47 +1,14 @@
-import { Entity } from '~/core/entities/entity'
 import { UniqueEntityID } from '~/core/entities/unique-entity-id'
 import { Optional } from '~/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-interface QuestionCommentProps {
-  authorId: UniqueEntityID
+interface QuestionCommentProps extends CommentProps {
   answerId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-
+export class QuestionComment extends Comment<QuestionCommentProps> {
   get answerId() {
     return this.props.answerId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  get excerpt() {
-    return this.content.substring(0, 120).trimEnd().concat('...')
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
   }
 
   static create(
