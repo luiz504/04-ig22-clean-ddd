6af7f9f51bddb65de1020eb3c420651c321f3fff
Luiz <luiz504@hotmail.com>
2024-01-05 17:37:01 -0300

    feat: either class handling

--- src/domain/forum/application/use-cases/comment-answer/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/comment-answer/delete-answer-comment.ts
@@ -1,28 +1,31 @@
+import { Either, left, right } from '~/core/either'
 import { AnswerCommentsRepository } from '../../repositories/answer-comments.repository'
 
 interface DeleteAnswerCommentsUseCaseRequest {
   answerCommentId: string
   authorId: string
 }
-
+type DeleteAnswerCommentsUseCaseResponse = Either<string, object>
 export class DeleteAnswerCommentsUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
   async execute({
     answerCommentId,
     authorId,
-  }: DeleteAnswerCommentsUseCaseRequest): Promise<void> {
+  }: DeleteAnswerCommentsUseCaseRequest): Promise<DeleteAnswerCommentsUseCaseResponse> {
     const answerComment =
       await this.answerCommentsRepository.findById(answerCommentId)
 
     if (!answerComment) {
-      throw new Error('Answer comment not found.')
+      return left('Answer comment not found.')
     }
 
     if (authorId !== answerComment.authorId.toString()) {
-      throw new Error('Now Allowed.')
+      return left('Now Allowed.')
     }
 
     await this.answerCommentsRepository.delete(answerComment)
+
+    return right({})
   }
 }
