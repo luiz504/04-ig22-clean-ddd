dcfbd0caca97d3a3ce93df14c9ed9a1be610a6fe
Luiz <luiz504@hotmail.com>
2024-01-05 16:57:48 -0300

    feat: fetch answer comments useCase

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '~/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '~/domain/forum/application/repositories/answer-comments.repository'
 import { AnswerComment } from '~/domain/forum/enterprise/entities/answer-comment'
 
@@ -16,6 +17,17 @@ export class InMemoryAnswerCommentsRepository
     if (!item) return null
 
     return item
+  }
+
+  async findManyByAnswerId(
+    answerId: string,
+    { page }: PaginationParams,
+  ): Promise<AnswerComment[]> {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+
+    return answerComments
   }
 
   async delete(answerComment: AnswerComment) {
