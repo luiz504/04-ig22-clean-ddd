2c5e7bf1bb669fc3eae28c380907d6e03b9dc593
Luiz <luiz504@hotmail.com>
2024-01-05 16:52:48 -0300

    feat: fetch question comments useCase

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '~/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '~/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '~/domain/forum/enterprise/entities/question-comment'
 
@@ -16,6 +17,17 @@ export class InMemoryQuestionCommentsRepository
     if (!item) return null
 
     return item
+  }
+
+  async findManyByQuestionId(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+
+    return questionComments
   }
 
   async delete(questionComment: QuestionComment) {
