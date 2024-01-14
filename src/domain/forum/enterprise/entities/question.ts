17e0483451237e5616b0251e1268524aedfb5c44
Luiz <luiz504@hotmail.com>
2024-01-14 15:15:29 -0300

    feat: aggregate root class creation

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs'
 
-import { Entity } from '~/core/entities/entity'
 import { Optional } from '~/core/types/optional'
 import { UniqueEntityID } from '~/core/entities/unique-entity-id'
 import { Slug } from './value-objects/slug'
+import { AggregateRoot } from '~/core/entities/aggregate-root'
 
 export interface QuestionProps {
   authorId: UniqueEntityID
@@ -14,7 +14,7 @@ export interface QuestionProps {
   createdAt: Date
   updatedAt?: Date
 }
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   get authorId() {
     return this.props.authorId
   }
