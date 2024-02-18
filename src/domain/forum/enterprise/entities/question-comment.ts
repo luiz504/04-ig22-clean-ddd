2c3a765f2807fd3ddaa486f8e2c48b6bd46a72dc
luiz bueno <luiz504@hotmail.com>
2024-02-18 18:24:07 -0300

    feat: onQuestionCommentCreatedEvent

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,6 +1,7 @@
 import { UniqueEntityID } from '~/core/entities/unique-entity-id'
 import { Optional } from '~/core/types/optional'
 import { Comment, CommentProps } from './comment'
+import { QuestionCommentCreatedEvent } from '~/domain/forum/enterprise/events/question-comment-created-event'
 
 export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID
@@ -19,6 +20,13 @@ export class QuestionComment extends Comment<QuestionCommentProps> {
       { ...props, createdAt: props.createdAt ?? new Date() },
       id,
     )
+    const isNewQuestionComment = !id
+
+    if (isNewQuestionComment) {
+      questionComment.addDomainEvent(
+        new QuestionCommentCreatedEvent(questionComment),
+      )
+    }
 
     return questionComment
   }
