8f76fa99c33170e27367c7c2d2da1ede7ec2cfc6
luiz bueno <luiz504@hotmail.com>
2024-02-18 12:15:24 -0300

    test: writting testes to send-notification

--- src/domain/notification/application/subscribers/on-answer-created.ts
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -1,9 +1,14 @@
 import { DomainEvents } from '~/core/events/domain-events'
 import { EventHandler } from '~/core/events/event-handler'
+import { QuestionsRepository } from '~/domain/forum/application/repositories/question-repository'
 import { AnswerCreatedEvent } from '~/domain/forum/enterprise/events/answer-created-event'
+import { SendNotificationUseCase } from '~/domain/notification/application/use-cases/send-notification'
 
 export class OnAnswerCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private questionRepository: QuestionsRepository,
+    private sendNotification: SendNotificationUseCase,
+  ) {
     this.setupSubscriptions()
   }
 
@@ -15,6 +20,17 @@ export class OnAnswerCreated implements EventHandler {
   }
 
   private async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {
-    console.log('Notification', answer)
+    const question = await this.questionRepository.findById(
+      answer.questionId.toString(),
+    )
+    if (question) {
+      await this.sendNotification.execute({
+        recipientId: question.authorId.toString(),
+        title: `New Answer at "${question.title
+          .substring(0, 40)
+          .concat('...')}"`,
+        content: answer.excerpt,
+      })
+    }
   }
 }
